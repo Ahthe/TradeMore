7961f480f98297e4521caa265b575f6a9f7b0b8b
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-22 07:33:39 -0400

    updated the vercel link for home page

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,7 +7,7 @@
 
 <nav class='items-center border-b border-gray-light flex flex-grow py-2 px-4 '>
     <a class='inline-flex items-center font-bold hover:underline active:underline'
-       href='https://myportfolio-ahthe.vercel.app/'>
+       href='https://https://trade-more.vercel.app'>
         <svg class='inline-block mr-1' height='16' width='16'>
             <circle cx='8' cy='8' fill='#ffd803' r='6' stroke='black' stroke-width='2' />
         </svg>
